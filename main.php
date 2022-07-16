bf8a6169870495d7b38a5390715070ac35ce657b
blackwhitebear <maxonleb5@gmail.com>
2022-07-16 17:09:18 +0300

    Fix telegram bot

--- main.php
+++ main.php
@@ -1,12 +1,11 @@
 #!/usr/bin/env php
 <?php
 require __DIR__ . '/vendor/autoload.php';
-require  __DIR__ . '/UserNumbers.php';
 
 use Longman\TelegramBot\Entities\Update;
 use Longman\TelegramBot\Request;
 
-$bot_api_key  = '5492344744:AAG6s-i2dHaRfpA900m_Upv509h_qjqyci4';
+
 $bot_username = '@sumismadeby_bot';
 
 
@@ -15,61 +14,52 @@ try {
     $telegram = new Longman\TelegramBot\Telegram($bot_api_key, $bot_username);
 
     $mysql_credentials = [
-        'host'     => 'localhost',
-        'user'     => 'root',
+        'host' => 'localhost',
+        'user' => 'root',
         'password' => 'Cra5hLoy@ale',
         'database' => 'telegram',
     ];
 
     $telegram->enableMySql($mysql_credentials);
 
-    // Handle telegram getUpdates request
+    $sql = mysqli_connect('localhost', 'root', 'Cra5hLoy@ale', 'telegram');
     $response = $telegram->handleGetUpdates();
     $result = $response->getResult();
-    /** @var \Longman\TelegramBot\Entities\Update  $update */
-    foreach($result as $update) {
+    /** @var \Longman\TelegramBot\Entities\Update $update */
+    foreach ($result as $update) {
         $message = $update->getMessage();
         $text = $message->getText();
+        $chat_id = $update->getMessage()->getChat()->getId();
+        $message_id = $update->getMessage()->getMessageId();
+        $previous_message_id = $message_id - 2;
+        $previous_message = mysqli_fetch_assoc($sql->query("select * from message where id=$previous_message_id and chat_id=$chat_id"));
+        var_dump($previous_message);
 
-        $database = new UserNumbers();
-        $userNumbers = $database->find($update->getMessage()->getChat()->getId());
-
-        $userNumbersData = $database->find($update->getMessage()->getChat()->getId(), true);
-        var_dump($userNumbersData);
-        echo $update->getMessage()->getMessageId();
         if ($text == "/start") {
-            if ($userNumbers === null){
-                $database->create($update->getMessage()->getChat()->getId());
-            }
-
             \Longman\TelegramBot\Request::sendMessage([
-                "chat_id" => $update->getMessage()->getChat()->getId(),
+                "chat_id" => $chat_id,
                 "text" => "Введите первое число"
             ]);
             continue;
         }
-        if ($userNumbersData["first_number"] === null) {
-            $userNumbers->update("first_number", intval($text));
+        if ($previous_message['text'] == "/start") {
             \Longman\TelegramBot\Request::sendMessage([
-                "chat_id" => $update->getMessage()->getChat()->getId(),
+                "chat_id" => $chat_id,
                 "text" => "Введите второе число"
             ]);
             continue;
-        } else {
-            if ($userNumbersData["sum"] === null) {
-                $sum = $userNumbersData["first_number"] + intval($text);
-                $userNumbers->update("sum", $sum);
-                \Longman\TelegramBot\Request::sendMessage([
-                    "chat_id" => $update->getMessage()->getChat()->getId(),
-                    "text" => "Сумма равна:$sum"
-
-                ]);
-            }
-            if (!$userNumbersData["sum"] === null and !$userNumbersData["first_number"] === null) {
-                $userNumbers->delete();
-            }
+        }
+        if ($previous_message['text'] != "/start") {
+            $x = intval($previous_message['text']);
+            $y = intval($text);
+            $sum = $x + $y;
+            \Longman\TelegramBot\Request::sendMessage([
+                "chat_id" => $chat_id,
+                "text" => "Сумма:$sum"
+            ]);
+            continue;
         }
     }
 } catch (Longman\TelegramBot\Exception\TelegramException $e) {
-        echo  $e->getMessage();
+    echo $e->getMessage();
 }
